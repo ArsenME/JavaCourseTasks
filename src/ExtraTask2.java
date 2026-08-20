88c6ac08bed311becee976b4c13e278e921d487d
Mac <mac@Macs-MacBook-Pro.local>
2026-08-20 20:18:41 +0400

    with SecondMax

--- src/ExtraTask2.java
+++ src/ExtraTask2.java
@@ -1,29 +1,40 @@
 public class ExtraTask2 {
     public static void main(String[] args) {
-//        int[] array1 = {15, 7, 15, 9, 7};
-//        int range = array1[0];
-//        for (int i = 1; i < array1.length; i++) {
-//            range = range ^ array1[i];
-//        }
-//        System.out.println(range);
+        int[] array1 = {15, 7, 15, 9, 7};
+        int range = array1[0];
+        for (int i = 1; i < array1.length; i++) {
+            range = range ^ array1[i];
+        }
+        System.out.println(range);
 
-//        int[] arr = {1, 2, 4, 22, 43, 53, 343, 553, 234, 321, 343, 2, 234, 4342, 423, 4};
-//        int max = arr[0];
-//        for (int i = 1; i < arr.length; i++) {
-//            if (arr[i] > max) {
-//                max = arr[i];
-//            }
-//
-//        }
-//        System.out.println(max);
-//        int min = arr[0];
-//        for (int i = 1; i < arr.length; i++) {
-//            if (arr[i] < min) {
-//                min = arr[i];
-//            }
-//
-//        }
-//        System.out.println(min);
+        int[] arr = {55555, 2, 4, 22, 43, 53, 343, 553, 234, 321, 343, 2, 234, 4342, 423, 4};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+
+        }
+        int secondMax = 0;
+        for (int i = 1; i < arr.length - 1; i+=2) {
+            if (max - arr[i] < max - arr[i+1] && max - arr[i] != 0  ) {
+                secondMax = arr[i];
+            }
+
+
+        }
+        System.out.println(secondMax);
+
+
+        System.out.println(max);
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
 
         int[][] matrix = {{1, 2, 3, 1}, {4, 5, 6, 2}, {7, 8, 9, 3}, {8, 5, 9, 4}};
         for (int i = 0; i < matrix.length; i++) {
@@ -85,5 +96,5 @@ int iteration = 0;
 
 
 
-//zangvaci mej amenamecy ev erkrord mecy tpel
+
 
